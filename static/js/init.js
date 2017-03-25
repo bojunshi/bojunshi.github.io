36d2e7c44a089efd2375ccde6428c368cbe40697
bojunshi <L02162010@gmail.com>
2017-03-25 15:27:25 +0800

    樣品

---
+++ static/js/init.js
@@ -0,0 +1,17 @@
+import $ from 'jquery'
+require('../bower_components/materialize/dist/js/materialize')
+export default {
+  mounted () {
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+    console.log($('.button-collapse'))
+  }
+}
+// (function($){
+//   $(function(){
+
+    
+
+//   }); // end of document ready
+// })(jQuery); // end of jQuery name space
